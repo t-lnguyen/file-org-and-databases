1618786d1f1a9b00ec01f8fb709aeced965d7e28
Thang Nguyen <tomn2009@gmail.com>
2021-11-19 12:45:20 -0600

    Added screen caps of averages; Added spreadsheet for capturing averages

--- Homework/Module11/Module11-HW-Q4.sql
+++ Homework/Module11/Module11-HW-Q4.sql
@@ -1,10 +1,19 @@
+-- Create the tables 
 Create table t1 ( a integer, b integer );
 Create table t2 ( b integer, c integer );
 Create table t3 ( c integer, d integer ); 
 
-insert into t1 values(1, 2);
-insert into t2 values(1, 2);
-insert into t3 values(1, 2);
+-- Insert some testing data
+insert into t1 values(10, 2);
+insert into t2 values(10, 2);
+insert into t3 values(10, 2);
+
+-- Show each of the tables' data
+select * from t1;
+select * from t2;
+select * from t3;
+
+
 
 explain select a from t1 
 where mod(a,5) = 0 and b in 
